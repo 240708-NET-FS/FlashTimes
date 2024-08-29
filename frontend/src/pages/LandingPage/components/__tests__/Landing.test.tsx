2d7a40be46094623eda7f907a76c59d842b2813b
erichards831 <lizrich831@gmail.com>
2024-08-29 15:11:44 -0400

    woops! forgot this

---
+++ frontend/src/pages/LandingPage/components/__tests__/Landing.test.tsx
@@ -0,0 +1,24 @@
+// Landing.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { describe, test, expect, it, jest } from "@jest/globals";
+
+import '@testing-library/jest-dom/extend-expect'; // for better assertions
+import Landing from 'pages/LandingPage/Landing';
+import { BrowserRouter } from 'react-router-dom';
+
+
+describe('Landing Component', () => {
+  test('renders without crashing', () => {
+    render(<BrowserRouter><Landing /></BrowserRouter>);
+    // Assertion to ensure Landing component is rendered
+    expect(screen.getByText(''));
+  });
+
+  test('renders FlashCard component', () => {
+    render(<BrowserRouter><Landing /></BrowserRouter>);
+    // Assuming FlashCard renders something identifiable
+    const flashCardElement = screen.getByRole('article'); // or another role if different
+    expect(flashCardElement);
+  });
+});
--- frontend/src/pages/LoginPage/Login.tsx
+++ frontend/src/pages/LoginPage/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { UserContext } from '../../App';
+import { UserContext } from '../../contexts/UserContext';
 import LoginBox from './components/LoginBox';
 
 const Login = ({}) => {
